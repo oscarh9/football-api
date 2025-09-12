caae0d7d3d9f38baac0e4da5463bc809086754ad
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-13 01:16:11 +0200

    Initialize Spring Boot project with base structure #1

---
+++ src/main/java/com/oscar/football_api/FootballApiApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.football_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballApiApplication.class, args);
+	}
+
+}
