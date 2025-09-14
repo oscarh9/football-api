7cb4ac0f773f08fc9df973ff9ef9afff7367cfd5
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-15 00:41:19 +0200

    Add League and Position enums #6

---
+++ src/main/java/com/oscar/football_api/entity/enums/League.java
@@ -0,0 +1,10 @@
+package com.oscar.football_api.entity.enums;
+
+
+public enum League {
+    PREMIER_LEAGUE,
+    LA_LIGA,
+    SERIE_A,
+    BUNDESLIGA,
+    LIGUE_1
+}
---
+++ src/main/java/com/oscar/football_api/entity/enums/Position.java
@@ -0,0 +1,8 @@
+package com.oscar.football_api.entity.enums;
+
+public enum Position {
+    GOALKEEPER,
+    DEFENDER,
+    MIDFIELDER,
+    FORWARD
+}
