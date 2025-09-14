0215667a0eedc2572c580c80740ef8a2587523eb
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-15 00:41:54 +0200

    Add JPA repositories for Club, Player and Manager #6

---
+++ src/main/java/com/oscar/football_api/repository/ManagerRepository.java
@@ -0,0 +1,9 @@
+package com.oscar.football_api.repository;
+
+import com.oscar.football_api.entity.Manager;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ManagerRepository extends JpaRepository<Manager, Long> {
+}
