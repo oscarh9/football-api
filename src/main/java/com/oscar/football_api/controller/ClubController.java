9a818233824fb35d989247bee37718f6f92674f2
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-15 14:03:21 +0200

    add createClub endpoint #8

---
+++ src/main/java/com/oscar/football_api/controller/ClubController.java
@@ -0,0 +1,27 @@
+package com.oscar.football_api.controller;
+
+import com.oscar.football_api.dto.ClubRequestDTO;
+import com.oscar.football_api.dto.response.ClubResponseDTO;
+import com.oscar.football_api.service.ClubService;
+import com.oscar.football_api.utils.ApiConstant;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ApiConstant.API + ApiConstant.V1 + ApiConstant.CLUBS)
+@RequiredArgsConstructor
+public class ClubController {
+
+    private final ClubService clubService;
+
+    @PostMapping
+    public ResponseEntity<ClubResponseDTO> createClub(@Valid @RequestBody ClubRequestDTO requestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clubService.createClub(requestDTO));
+    }
+}
