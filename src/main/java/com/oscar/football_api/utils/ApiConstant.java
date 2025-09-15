c26341ad8835a0193d8c667bb100b88df6665d8c
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-15 14:02:35 +0200

    add ApiConstant file #8

---
+++ src/main/java/com/oscar/football_api/utils/ApiConstant.java
@@ -0,0 +1,14 @@
+package com.oscar.football_api.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ApiConstant {
+
+    public static final String API = "/api";
+    public static final String V1 = "/v1";
+    public static final String CLUBS = "/clubs";
+    public static final String PLAYERS = "/players";
+    public static final String MANAGERS = "/managers";
+
+}
