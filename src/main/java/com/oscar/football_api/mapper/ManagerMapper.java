f6ab1837a2e6d6aab2a0577069d5fd72bc3300e3
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-15 16:08:04 +0200

    create ManagerMapper #8

---
+++ src/main/java/com/oscar/football_api/mapper/ManagerMapper.java
@@ -0,0 +1,17 @@
+package com.oscar.football_api.mapper;
+
+import com.oscar.football_api.dto.ManagerRequestDTO;
+import com.oscar.football_api.dto.response.ManagerResponseDTO;
+import com.oscar.football_api.entity.Manager;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ManagerMapper {
+
+    @Mapping(source = "clubId", target = "club.id")
+    Manager toEntity(ManagerRequestDTO dto);
+
+    @Mapping(source = "club.id", target = "clubId")
+    ManagerResponseDTO toDTO(Manager manager);
+}
