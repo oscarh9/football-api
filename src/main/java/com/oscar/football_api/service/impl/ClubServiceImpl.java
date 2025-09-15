444ef372283f1330875481701172dd343e04c7a5
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-15 16:51:44 +0200

    implement @Transactional annotation in ClubServiceImpl #8

--- src/main/java/com/oscar/football_api/service/impl/ClubServiceImpl.java
+++ src/main/java/com/oscar/football_api/service/impl/ClubServiceImpl.java
@@ -6,6 +6,7 @@ import com.oscar.football_api.entity.Club;
 import com.oscar.football_api.mapper.ClubMapper;
 import com.oscar.football_api.repository.ClubRepository;
 import com.oscar.football_api.service.ClubService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +15,7 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ClubServiceImpl implements ClubService {
 
     private final ClubRepository clubRepository;
