b707da30786b968c598276fc455e788158a147e5
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-16 03:04:29 +0200

    Add @Tag and @Operation annotations to Controllers #10

--- src/main/java/com/oscar/football_api/controller/ClubController.java
+++ src/main/java/com/oscar/football_api/controller/ClubController.java
@@ -4,6 +4,8 @@ import com.oscar.football_api.dto.ClubRequestDTO;
 import com.oscar.football_api.dto.response.ClubResponseDTO;
 import com.oscar.football_api.service.ClubService;
 import com.oscar.football_api.utils.ApiConstant;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,31 +17,38 @@ import java.util.List;
 @RestController
 @RequestMapping(ApiConstant.API + ApiConstant.V1 + ApiConstant.CLUBS)
 @RequiredArgsConstructor
+@Tag(name = "Clubs", description = "Endpoints for club management")
+
 public class ClubController {
 
     private final ClubService clubService;
 
     @PostMapping
+    @Operation(summary = "Create a new club")
     public ResponseEntity<ClubResponseDTO> createClub(@Valid @RequestBody ClubRequestDTO requestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(clubService.createClub(requestDTO));
     }
 
     @GetMapping
+    @Operation(summary = "Get all clubs")
     public ResponseEntity<List<ClubResponseDTO>> getALlClubs() {
         return ResponseEntity.ok(clubService.getAllClubs());
     }
 
     @GetMapping(ApiConstant.ID)
+    @Operation(summary = "Get one club")
     public ResponseEntity<ClubResponseDTO> getClubById(@PathVariable Long id) {
         return ResponseEntity.ok(clubService.getClubById(id));
     }
 
     @PutMapping(ApiConstant.ID)
+    @Operation(summary = "Update a club")
     public ResponseEntity<ClubResponseDTO> updateClub(@PathVariable Long id, @Valid @RequestBody ClubRequestDTO requestDTO) {
         return ResponseEntity.ok(clubService.updateClub(id, requestDTO));
     }
 
     @DeleteMapping(ApiConstant.ID)
+    @Operation(summary = "Delete a club")
     public ResponseEntity<Void> deleteClub(@PathVariable Long id) {
         clubService.deleteClub(id);
         return ResponseEntity.noContent().build();
--- src/main/java/com/oscar/football_api/controller/PlayerController.java
+++ src/main/java/com/oscar/football_api/controller/PlayerController.java
@@ -4,6 +4,8 @@ import com.oscar.football_api.dto.PlayerRequestDTO;
 import com.oscar.football_api.dto.response.PlayerResponseDTO;
 import com.oscar.football_api.service.PlayerService;
 import com.oscar.football_api.utils.ApiConstant;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -15,31 +17,37 @@ import java.util.List;
 @RestController
 @RequestMapping(ApiConstant.API + ApiConstant.V1 + ApiConstant.PLAYERS)
 @RequiredArgsConstructor
+@Tag(name = "Players", description = "Endpoints for player management")
 public class PlayerController {
 
     private final PlayerService playerService;
 
     @PostMapping
+    @Operation(summary = "Create a new player")
     public ResponseEntity<PlayerResponseDTO> createPlayer(@Valid @RequestBody PlayerRequestDTO requestDTO) {
         return ResponseEntity.status(HttpStatus.CREATED).body(playerService.createPlayer(requestDTO));
     }
 
     @GetMapping
+    @Operation(summary = "Get all players")
     public ResponseEntity<List<PlayerResponseDTO>> getAllPlayers() {
         return ResponseEntity.ok(playerService.getAllPlayers());
     }
 
     @GetMapping(ApiConstant.ID)
+    @Operation(summary = "Get one player")
     public ResponseEntity<PlayerResponseDTO> getPlayerById(@PathVariable Long id) {
         return ResponseEntity.ok(playerService.getPlayerById(id));
     }
 
     @PutMapping(ApiConstant.ID)
+    @Operation(summary = "Update a player")
     public ResponseEntity<PlayerResponseDTO> updatePlayer(@PathVariable Long id, @Valid @RequestBody PlayerRequestDTO requestDTO) {
         return ResponseEntity.ok(playerService.updatePlayer(id, requestDTO));
     }
 
     @DeleteMapping(ApiConstant.ID)
+    @Operation(summary = "Delete a player")
     public ResponseEntity<Void> deletePlayer(@PathVariable Long id) {
         playerService.deletePlayer(id);
         return ResponseEntity.noContent().build();
