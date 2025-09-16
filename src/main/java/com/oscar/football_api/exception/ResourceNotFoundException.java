4d7664e260c9b645d638d6d2fb6159c0cb27518b
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-16 16:24:47 +0200

    add custom exceptions for global error handling #12

---
+++ src/main/java/com/oscar/football_api/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.oscar.football_api.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
