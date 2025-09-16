0401c18f5b1dbbdb889a02da5533984bcf375d46
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-16 16:25:45 +0200

    create ErrorMessage for consistent error responses #12

---
+++ src/main/java/com/oscar/football_api/exception/ErrorMessage.java
@@ -0,0 +1,19 @@
+package com.oscar.football_api.exception;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class ErrorMessage {
+
+    private final String error;
+    private final String message;
+    private final Integer code;
+
+    public ErrorMessage(Exception exception, Integer code) {
+        this.error = exception.getClass().getSimpleName();
+        this.message = exception.getMessage();
+        this.code = code;
+    }
+}
