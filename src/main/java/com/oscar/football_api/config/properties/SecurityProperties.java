c882a09ce93569748a8e579f349cd9806f67a9e5
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-20 03:18:30 +0200

    add configuration properties classes for OpenApi and Security and centralized in ApplicationConfiguration #16

---
+++ src/main/java/com/oscar/football_api/config/properties/SecurityProperties.java
@@ -0,0 +1,17 @@
+package com.oscar.football_api.config.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "security-properties")
+public class SecurityProperties {
+
+    private Admin admin = new Admin();
+
+    @Data
+    public static class Admin {
+        private String username;
+        private String password;
+    }
+}
