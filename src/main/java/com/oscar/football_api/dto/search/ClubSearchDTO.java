b63cca0903cbe08c23e179aa69da70ad45ca6616
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-22 04:06:13 +0200

    add ClubSearchDTO for filtering and pagination in clubs searches #18

---
+++ src/main/java/com/oscar/football_api/dto/search/ClubSearchDTO.java
@@ -0,0 +1,21 @@
+package com.oscar.football_api.dto.search;
+
+import com.oscar.football_api.entity.enums.League;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClubSearchDTO {
+
+    private String name;
+    private String city;
+    private String stadiumName;
+    private League league;
+    private String sortBy = "name";
+    private String sortDir = "asc";
+    private int page = 0;
+    private int size = 10;
+}
