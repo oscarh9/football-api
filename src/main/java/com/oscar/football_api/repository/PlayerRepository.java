7132f0bbdf32fdbf779acfadbc6811dadded9d9f
oscarh9 <oscarherenciasakkis@gmail.com>
2025-09-22 02:44:49 +0200

    update PlayerRepository extending JpaSpecificationExecutor #18

--- src/main/java/com/oscar/football_api/repository/PlayerRepository.java
+++ src/main/java/com/oscar/football_api/repository/PlayerRepository.java
@@ -2,12 +2,13 @@ package com.oscar.football_api.repository;
 
 import com.oscar.football_api.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface PlayerRepository extends JpaRepository<Player, Long> {
+public interface PlayerRepository extends JpaRepository<Player, Long>, JpaSpecificationExecutor<Player> {
 
     Optional<Player> findByClubIdAndJerseyNumber(Long clubId, int jerseyNumber);
 }
